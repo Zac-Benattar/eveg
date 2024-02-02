7b7ef9acdb2255c70bb0e2f24eeb64764a666131
Zac <zbenattar@gmail.com>
2024-02-02 21:27:10 +0000

    Layout improvements

--- my-skeleton-app/src/routes/+page.svelte
+++ my-skeleton-app/src/routes/+page.svelte
@@ -11,8 +11,44 @@
 	});
 </script>
 
+<<<<<<< Updated upstream
 <div>
 	<div><h1>Fresh Food</h1></div>
+=======
+<div class="flex flex-col p-5">
+	<div class="flex flex-row place-content-center"><div class="h2">Fresh Food</div></div>
+	<div class="flex flex-row place-content-center gap-8 p-3">
+		<label class="label">
+			<span>Filters</span>
+			<div class="flex flex-row gap-2">
+				{#each Object.keys(categories) as f}
+					<button
+						class="chip {categories[f] ? 'variant-filled bg-surface-500' : 'variant-soft'}"
+						on:click={() => {
+							toggle(f);
+						}}
+						on:keypress
+					>
+						{#if categories[f]}<span>✔️</span>{/if}
+						<span class="capitalize">{f}</span>
+					</button>
+				{/each}
+			</div>
+		</label>
+		<div>
+			<label class="label">
+				<span>Sort by:</span>
+				<select class="select" id="sort-by">
+					<option value="1">Alphabetical</option>
+					<option value="2">Category</option>
+					<option value="3">Price High to Low</option>
+					<option value="4">Price Low to High</option>
+				</select>
+			</label>
+		</div>
+	</div>
+
+>>>>>>> Stashed changes
 	<div class="container h-full mx-auto flex justify-center items-center">
 		<div class="space-y-10 text-center flex flex-col items-center">
 			<ProductsGrid products={productList} />
