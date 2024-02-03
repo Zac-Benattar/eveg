e5ffc59023aee110a7a4a54a6abdf2ba5c8c375d
Zac <zbenattar@gmail.com>
2024-02-03 11:14:07 +0000

    Fixed grid layout

--- my-skeleton-app/src/routes/+page.svelte
+++ my-skeleton-app/src/routes/+page.svelte
@@ -24,21 +24,38 @@
 </script>
 
 <div class="flex flex-col p-2">
-	<div class="h2"><h1>Fresh Food</h1></div>
-	<div class="flex flex-row gap-2">
-		{#each Object.keys(categories) as f}
-			<button
-				class="chip {categories[f] ? 'variant-filled' : 'variant-soft'}"
-				on:click={() => {
-					toggle(f);
-				}}
-				on:keypress
-			>
-				{#if categories[f]}<span>✔️</span>{/if}
-				<span class="capitalize">{f}</span>
-			</button>
-		{/each}
+	<div class="h2 flex flex-row place-content-center">Fresh Food</div>
+	<div class="flex flex-row place-content-center p-2 gap-x-5">
+		<label class="label">
+			<span>Filter by:</span>
+			<div class="flex flex-row gap-2">
+				{#each Object.keys(categories) as f}
+					<button
+						class="chip {categories[f] ? 'variant-filled' : 'variant-soft'}"
+						on:click={() => {
+							toggle(f);
+						}}
+						on:keypress
+					>
+						{#if categories[f]}<span>✔️</span>{/if}
+						<span class="capitalize">{f}</span>
+					</button>
+				{/each}
+			</div>
+		</label>
+		<div>
+			<label class="label">
+				<span>Sort by:</span>
+				<select class="select" id="sort-by">
+					<option value="1">Alphabetical</option>
+					<option value="2">Category</option>
+					<option value="3">Price High to Low</option>
+					<option value="4">Price Low to High</option>
+				</select>
+			</label>
+		</div>
 	</div>
+
 	<div class="container h-full mx-auto flex justify-center items-center">
 		<div class="space-y-10 text-center flex flex-col items-center">
 			<ProductsGrid products={productList} />
